2edda474520ecdf36f4ab3135248e6c57e2c4c4b
Santeri Ruuskanen <81624691+sandelsson@users.noreply.github.com>
2021-09-30 19:58:15 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# WebApp_week3
-Created with CodeSandbox
+# vanilla-template
+
+This is a template for weekly assignments.
---
+++ src/index.js
@@ -0,0 +1,62 @@
+import "./styles.css";
+/*
+if (document.readyState !== "loading") {
+  initializeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    initializeCode();
+  });
+}
+
+function initializeCode() {
+  document.getElementById("app").innerHTML = "<h1>Hello!</h1>";
+}
+*/
+
+var container_div = document.createElement("div");
+container_div.className = "container";
+
+for (let x = 0; x < 5; x++) {
+  fetch("https://dog.ceo/api/breed/chihuahua/images/random")
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log(data);
+      let picture = data.message;
+      let breed = "Chihuahua";
+      //console.log(picture);
+      create_elements(breed, picture);
+    });
+}
+
+function create_elements(breed, picture) {
+  var item = document.createElement("div");
+  item.className = "wiki-item";
+
+  var header = document.createElement("h1");
+  header.className = "wiki-header";
+  header.innerHTML = breed;
+
+  var content = document.createElement("div");
+  content.className = "wiki-content";
+
+  var text = document.createElement("p");
+  text.className = "wiki-text";
+
+  var container = document.createElement("div");
+  container.className = "img-container";
+
+  var img = document.createElement("img");
+  img.className = "wiki-img";
+  img.src = picture;
+
+  container.appendChild(img);
+
+  content.appendChild(text);
+  content.appendChild(container);
+
+  item.appendChild(header);
+  item.appendChild(content);
+
+  container_div.appendChild(item);
+  document.body.appendChild(container_div);
+}
